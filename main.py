9263893fd1c05d8b26a2d226a6a895f797b5a8b5
jhlemos <santos.jordao@escola.pr.gov.br>
2025-08-11 11:02:33 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+peso = float(input("Digite o seu peso"))
+altura = float(input("Digite a sua altura em metros"))
+
+inc = peso / (altura + altura)
+
+if (imc < 18,5)
+   print("abaixo do peso")
+   elif ()
